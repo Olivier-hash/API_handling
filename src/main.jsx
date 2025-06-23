7122ac13212ccab545020896984de75033a270c4
olivier Rukundo <rr.olivier01@gmail.com>
2025-06-23 21:32:22 +0200

    wraped App with Redux Provider and ThemeProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { Provider } from 'react-redux';
+import { ThemeProvider } from './context/ThemeContext';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.render(
+  <Provider store={store}>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </Provider>,
+  document.getElementById('root')
+);
